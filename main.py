94ea9cc07de05506e95b956b55c9d0835f6793ff
ProfessionalJayLi <129302306+ProfessionalJayLi@users.noreply.github.com>
2023-04-05 10:56:29 -0400

    Update main.py
    followed tutorial "The ultimate introduction to Pygame" by Clear Code, added frame rate and display surface

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 python3 -m pip install pygame
 import pygame
+from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((1950, 1000))
+pygame.display.set_caption('Sword\'s End')
+clock = pygame.item.Clock()
+
+while True:
+  for even in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      exit()
+  pygame.display.update()
+  clock.tick(60)
