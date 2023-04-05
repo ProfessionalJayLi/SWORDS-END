97a3c8105980527755a6f89a18ae302a996e338e
ProfessionalJayLi <129302306+ProfessionalJayLi@users.noreply.github.com>
2023-04-05 11:00:14 -0400

    Update main.py
    Hehe

--- main.py
+++ main.py
@@ -7,10 +7,15 @@ screen = pygame.display.set_mode((1950, 1000))
 pygame.display.set_caption('Sword\'s End')
 clock = pygame.item.Clock()
 
+# Following Tutorial
+test_siurface = pygame.Surface((100,200)) 
+
 while True:
   for even in pygame.event.get():
     if event.type == pygame.QUIT:
       pygame.quit()
       exit()
+      
+  screen.blit(0, 0)
   pygame.display.update()
   clock.tick(60)
