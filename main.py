59b20ffbf22b54e4d5f9b3e41653772f4900c912
ProfessionalJayLi <129302306+ProfessionalJayLi@users.noreply.github.com>
2023-04-21 10:17:33 -0400

    Update main.py
    bruh

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
+```python
+
+# Level and Game class from Creating a Zelda style game in Python by Clear Code
+
 python3 -m pip install pygame
 import pygame
-from sys import exit
+from sys import exit  
 
-pygame.init()
-screen = pygame.display.set_mode((1950, 1000))
-pygame.display.set_caption('Sword\'s End')
-clock = pygame.time.Clock()
+class Game:
+    def __init__(self):
+        pygame.init()
+        screen = pygame.display.set_mode((1950, 1000))
+        pygame.display.set_caption('Sword\'s End')
+        clock = pygame.time.Clock()
+        
+        self.level = Level()
 
-# Following Tutorial
+    def run(self):
+        while True:
+          for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+              pygame.quit()
+              sys.exit()
 
-while True:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      pygame.quit()
-      exit()
-      
-  pygame.display.update()
-  clock.tick(60)
+          self.screen.fill('black')
+          self.level.run()
+          pygame.display.update()
+          clock.tick(60)
+            
+if __name__ == '__main__':
+    game = Game()
+    game.run()
+```
