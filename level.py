e726bc200d0d24d55bcf1046cb26e7da427958bd
ProfessionalJayLi <129302306+ProfessionalJayLi@users.noreply.github.com>
2023-04-22 14:24:21 -0400

    Update level.py

--- level.py
+++ level.py
@@ -1,9 +1,15 @@
 ``` python
+import pygame
 class Level:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
         self.visible_sprites = pygame.sprite.Group() #find out what the fuck sprite.Group() does
         self.obstacles_sprites = pygame.sprite.Group()
+        self.create_map()
+        
+    def create_map(self):
+        for row_index,row in enumerate(WORLD_MAP):
+            print(row)
         
     def run(self):
         pass
