5036f9996b06277420b9eb94cff939b8e7238d46
ProfessionalJayLi <129302306+ProfessionalJayLi@users.noreply.github.com>
2023-05-11 20:22:10 -0400

    Create controls.py

---
+++ controls.py
@@ -0,0 +1,9 @@
+for event in pygame.event.get():
+    if event.type == pygame.KEYDOWN or event.type == pygame.K_s:
+      pass
+    if event.type == pygame.KEYUP or event.type == pygame.K_w:
+      pass
+    if event.type == pygame.KEYLEFT or event.type == pygame.K_a:
+      pass
+    if event.type == pygame.KEYRIGHT or event.type == pygame.K_d:
+      pass
