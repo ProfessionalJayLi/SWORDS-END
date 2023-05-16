8ac2e3346a3e39ceed1527780496f93d0e174d9d
ProfessionalJayLi <129302306+ProfessionalJayLi@users.noreply.github.com>
2023-05-16 14:14:03 -0400

    Create tile.py

---
+++ tile.py
@@ -0,0 +1,11 @@
+``` python
+import pygame
+from settings import *
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().init(groups)
+        self.image = pygame.image.load('graphics/floor.png')
+        self.rect = self.image.get_rect(topleft = pos)
+                     
+```
