ef349fded9df3b49483d4db715c6cd10529da749
ProfessionalJayLi <129302306+ProfessionalJayLi@users.noreply.github.com>
2023-05-16 14:41:01 -0400

    Update characters.py

--- characters.py
+++ characters.py
@@ -11,8 +11,25 @@ class Player(pygame.sprite.Sprite):
         self.direction = pygame.math.Vector2():
             
     def input(self):
-        keys = pygame.key
-                     
+        keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+        
+        if keys[pygame.K_RIGHT or keys[pygame.K_d]]:
+            self.direction.x = -1
+        elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+            
+    def update(self):
+        self.input()
+        
 ```
 
 '''
