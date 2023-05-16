a3f09e94c80eff1884f1a665ae882941d67e698d
ProfessionalJayLi <129302306+ProfessionalJayLi@users.noreply.github.com>
2023-05-16 14:34:29 -0400

    Update level.py

--- level.py
+++ level.py
@@ -8,7 +8,7 @@ from characters import Player
 class Level:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
-        self.visible_sprites = pygame.sprite.Group() #find out what the fuck sprite.Group() does
+        self.visible_sprites = pygame.sprite.Group() 
         self.obstacles_sprites = pygame.sprite.Group()
         self.create_map()
         
@@ -18,8 +18,13 @@ class Level:
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
                 if col == 'x':
-                    
+                    Wall((x,y),[self.visible_sprites])
+                if col == ' ':
+                    Tile((x,y),[self.visible_sprites])
+                if col == 'p':
+                    Player((x,y),[self.visible_sprites])
         
     def run(self):
-        pass
+        self.visible_sprites.draw(self.display_surface)
+        
 ```
