39eb19790f6134fea7457d18cb44266a93cb515e
ProfessionalJayLi <129302306+ProfessionalJayLi@users.noreply.github.com>
2023-05-16 14:25:33 -0400

    Update tile.py

--- tile.py
+++ tile.py
@@ -7,5 +7,11 @@ class Tile(pygame.sprite.Sprite):
         super().init(groups)
         self.image = pygame.image.load('graphics/floor.png')
         self.rect = self.image.get_rect(topleft = pos)
+        
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().init(groups)
+        self.image = pygame.image.load('graphics/wall.png')
+        self.rect = self.image.get_rect(topleft = pos)
                      
 ```
